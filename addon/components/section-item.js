8d4e661f770d6ba4a2cd56aae7c26db774cd8dc6
Bradley <b_leftley56@yahoo.co.uk>
2020-05-05 18:05:37 +0100

    Inital files

---
+++ addon/components/section-item.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+import layout from '../templates/components/section-item';
+
+export default Component.extend({
+  layout
+});
---
+++ app/components/nav-item.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-scroll-spy/components/nav-item';
