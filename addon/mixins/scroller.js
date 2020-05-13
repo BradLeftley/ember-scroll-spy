7ef16b20aa3e6b5d978c4dfd78913732ed1447cd
Bradley <b_leftley56@yahoo.co.uk>
2020-05-13 16:05:05 +0100

    Add mixin and service

--- addon/mixins/scroller.js
+++ addon/mixins/scroller.js
@@ -1,4 +1,24 @@
 import Mixin from '@ember/object/mixin';
+import $ from 'jquery';
 
 export default Mixin.create({
+  bindScrolling: function() {
+    document.addEventListener(
+      'scroll',
+      () => {
+        return this.scrolled();
+      },
+      true
+    );
+  },
+
+  unbindScrolling: function() {
+    document.removeEventListener(
+      'scroll',
+      () => {
+        return this.scrolled();
+      },
+      false
+    );
+  }
 });
--- addon/services/viewport-handler.js
+++ addon/services/viewport-handler.js
@@ -1,4 +1,18 @@
 import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+
 
 export default Service.extend({
+    store: service(),
+    current: null,
+    init() {
+      this._super(...arguments);
+      this.set('current', null);
+    },
+   
+    updateViewPortActivity(status, areaName) {
+      if(status === true) {
+         this.set('current', areaName);
+       } 
+     }
 });
