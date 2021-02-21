4db331f366022b6199a3b63e7c6beafeec0979a8
Bradley Leftley <bradleyleftley@Bradleys-MacBook-Pro-2.local>
2021-02-21 21:01:20 +0000

    fix optional feature

--- index.js
+++ index.js
@@ -2,10 +2,7 @@
 
 module.exports = {
   name: require('./package').name,
-<<<<<<< HEAD
 //   isDevelopingAddon: function() {
 //     return true;
 // },
-=======
->>>>>>> 476cef4... v3.9.0...v3.25.0
 };
