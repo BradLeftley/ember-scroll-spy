715cc8262cab474eff599cd26ed5a9cfaa898317
Adam Woźny <wozny1989@gmail.com>
2021-10-21 18:57:48 +0200

    fix: handle deprecation https://deprecations.emberjs.com/v3.x#toc_events-remove-all-listeners

--- addon/components/scroll-container.js
+++ addon/components/scroll-container.js
@@ -1,29 +1,36 @@
 import Component from '@ember/component';
-import { inject as service } from "@ember/service";
+import { inject as service } from '@ember/service';
 import layout from '../templates/components/scroll-container';
 
 export default Component.extend({
-    viewportHandler: service('viewport-handler'),
-    tagName: 'span',
+  viewportHandler: service('viewport-handler'),
+  tagName: 'span',
 
-    classNameBindings: ['active'],
-    layout,
-    
-    init(){
-        this._super(...arguments);
-        const linkId = this.linkId;
-        this.set(`viewportHandler.${linkId}`, false);
-        this.set('viewportHandler.current', null);
+  classNameBindings: ['active'],
+  layout,
 
-        //Check if in viewport then set 
-        this.set('active', this.viewportHandler.current === linkId);
+  init() {
+    this._super(...arguments);
+    const linkId = this.linkId;
+    this.set(`viewportHandler.${linkId}`, false);
+    this.set('viewportHandler.current', null);
 
-        this.addObserver(`viewportHandler.current`,function() {
-            this.set('active', this.viewportHandler.current === linkId);
-        });
-      },
-      willDestroy() {
-        this._super();
-        this.removeObserver(`viewportHandler.${this.linkId}`);
-      }
+    //Check if in viewport then set
+    this.set('active', this.viewportHandler.current === linkId);
+
+    this.addObserver(`viewportHandler.current`, this, this.setViewportHandler);
+  },
+
+  willDestroy() {
+    this._super();
+    this.removeObserver(
+      `viewportHandler.${this.linkId}`,
+      this,
+      this.setViewportHandler
+    );
+  },
+
+  setViewportHandler() {
+    this.set('active', this.viewportHandler.current === this.linkId);
+  },
 });
